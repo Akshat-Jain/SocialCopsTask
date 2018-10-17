56bb1235d4445443c0ade4954d2c404747a90556
Akshat-Jain <akjn11@gmail.com>
2018-10-17 23:30:19 +0530

    Added functionality to upload csv file

--- src/components/CSV/CSV.react.js
+++ src/components/CSV/CSV.react.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import CSVReader from 'react-csv-reader'
+import "./CSV.css";
 
 class CSV extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+
+  handleForce = (data) => {
+    this.setState({
+      data: data
+    })
+  };
+
   render() {
+    {console.log(this.state.data)}
     return (
-        <div>
-            hello
-        </div>
+        <div className="container">
+        <CSVReader
+          cssClass="react-csv-input"
+          label="Select CSV file with statistics required for the task"
+          onFileLoaded={this.handleForce}
+        />
+        <p>Once you have selected the CSV file, open the console.</p>
+      </div>
     );
   }
 }
